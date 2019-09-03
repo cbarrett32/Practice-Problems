4d240bd84fab8a6373495bb673302cf1edab4ba3
Charlie Barrett <37375131+cbarrett32@users.noreply.github.com>
2019-09-03 18:18:44 -0400

    Create TreeNode.java

---
+++ Array to Binary Search Tree/TreeNode.java
@@ -0,0 +1,12 @@
+public class TreeNode {
+	public int value;
+	public TreeNode[] children;
+	
+	public TreeNode(int value, TreeNode[] children) {
+		//super();
+		this.value = value;
+		this.children = children;
+	}
+	
+
+}
