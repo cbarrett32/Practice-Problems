00063a33ecf018c7df776dc2e0fd18aed91d244b
Charlie Barrett <37375131+cbarrett32@users.noreply.github.com>
2019-09-03 18:05:15 -0400

    Update Is_Permutation.java

--- Check Permutation/Is_Permutation.java
+++ Check Permutation/Is_Permutation.java
@@ -1,31 +1,31 @@
-	public static boolean isPermutation(String word1, String word2){
-		char[] word1sorted = word1.toCharArray();
-		Arrays.sort(word1sorted);
-		char[] word2sorted = word2.toCharArray();
-		Arrays.sort(word2sorted);
-		if(Arrays.equals(word1sorted, word2sorted))
-			return true;
+public static boolean isPermutation(String word1, String word2){
+	char[] word1sorted = word1.toCharArray();
+	Arrays.sort(word1sorted);
+	char[] word2sorted = word2.toCharArray();
+	Arrays.sort(word2sorted);
+	if(Arrays.equals(word1sorted, word2sorted))
+		return true;
+	else {
+		return false;
+	}
+}
+
+public static boolean isPermutation2(String word1, String word2){
+	String[] word1array = word1.split("");
+	List<String> myList = new ArrayList<String>(Arrays.asList(word1array));
+	String[] word2array = word2.split("");
+	for(String x: word2array){
+		if(myList.isEmpty()){
+			return false;
+		}
+		if(!myList.contains(x))
+			return false;
 		else {
-			return false;
+			myList.remove(x);
 		}
 	}
-	
-	public static boolean isPermutation2(String word1, String word2){
-		String[] word1array = word1.split("");
-		List<String> myList = new ArrayList<String>(Arrays.asList(word1array));
-		String[] word2array = word2.split("");
-		for(String x: word2array){
-			if(myList.isEmpty()){
-				return false;
-			}
-			if(!myList.contains(x))
-				return false;
-			else {
-				myList.remove(x);
-			}
-		}
-		if(myList.isEmpty())
-			return true;
-		else
-			return false;
-	}
+	if(myList.isEmpty())
+		return true;
+	else
+		return false;
+}
