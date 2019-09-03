ef4fc00231e2789661eac033ef847dfaaad799f9
Charlie Barrett <37375131+cbarrett32@users.noreply.github.com>
2019-09-03 18:09:28 -0400

    Update URLify.java

--- URLify/URLify.java
+++ URLify/URLify.java
@@ -1,1 +1,13 @@
-
+public static String URLify(String ogWord){
+  char[] ogWordArray = ogWord.toCharArray();
+  String x = "";
+  for (char y: ogWordArray){
+    if(Character.toString(y).equals(" ")){
+      x+="%20";
+    }
+    else{
+      x+=y;
+    }
+  }
+  return x;
+}
